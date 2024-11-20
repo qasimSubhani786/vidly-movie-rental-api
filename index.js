0517a6c02674748afbeab086aa0841494a72ed4e
Qasim Subhani <qasimsubhani786@gmail.com>
2024-11-20 18:23:31 +0500

    puhing code to repo

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 const winston = require("winston");
 
-require("./startup/logging")();
+require("./startup/logging")(); //
 require("./startup/routes")(app);
 require("./startup/db")();
-require("./startup/config")();
+require("./startup/config")(); //
 require("./startup/validation")();
 
 const port = process.env.PORT || 3000;
--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,6 @@ const jwt = require("jsonwebtoken");
 module.exports = function (req, res, next) {
   const token = req.header("x-auth-token");
   if (!token) return res.status(401).send("Access Denied! No Token Provided");
-
   try {
     const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
     req.user = decoded;
